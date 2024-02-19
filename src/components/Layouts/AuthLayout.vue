999b5fb3c812b6b883ad9069ac53d9117f88b904
kufre-abasi <kufreabasibassey3@gmail.com>
2024-02-20 00:20:42 +0100

    updated

--- src/components/Layouts/AuthLayout.vue
+++ src/components/Layouts/AuthLayout.vue
@@ -2,7 +2,7 @@
   <main class="bg-[#F0F2F5] h-full">
     <AuthPageNavbar />
     <div
-      class="w-full h-screen flex justify-center py-8 mb-20 items-center lg:px-[16px] hide-scrollbar"
+      class="w-full h-full flex justify-center py-12 !pb-20 mb-20 items-center lg:px-[16px] hide-scrollbar"
     >
       <slot></slot>
     </div>
