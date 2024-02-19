8c33709a046bb5ebfb0c5f689a074096cc872cf8
kufre-abasi <kufreabasibassey3@gmail.com>
2024-02-20 00:15:58 +0100

    feat: middleware

--- src/router/Middleware/index.js
+++ src/router/Middleware/index.js
@@ -1,76 +1,76 @@
-// import { decrypt } from '@/services/Encrypt'
-
-// const getUserFromLocalStorage = () => {
-//   const encryptedData = localStorage.getItem('_user_data')
-//   return encryptedData ? decrypt(encryptedData, import.meta.env.VITE_ENCRYPT_KEY).data : null
-// }
-
-// const redirectLogin = () => {
-//   const user = getUserFromLocalStorage()
-//   return user && user.token ? undefined : { name: 'login' }
-// }
-
-// const redirectDashboard = () => {
-//   const user = getUserFromLocalStorage()
-//   return user && user.token ? { name: 'dashboard' } : undefined
-// }
-
-// const deleteSession = () => {
-//   if (localStorage.getItem('_user_data')) {
-//     localStorage.removeItem('_user_data')
-//     return { name: 'login' }
-//   }
-//   return undefined
-// }
-// export default {
-//   redirectLogin,
-//   redirectDashboard,
-//   deleteSession
-// }
-
 import { decrypt } from '@/services/Encrypt'
 
+const getUserFromLocalStorage = () => {
+  const encryptedData = localStorage.getItem('_user_data')
+  return encryptedData ? decrypt(encryptedData, import.meta.env.VITE_ENCRYPT_KEY) : null
+}
+
 const redirectLogin = () => {
-
-    let encryptedData  = localStorage.getItem("_user_data");
-    if(encryptedData){
-        let user = decrypt(encryptedData,import.meta.env.VITE_ENCRYPT_KEY)
-
-        if(user.data.token){
-            return ;
-        }
-
-        return { name: 'login' }
-
-    }
-
-    return { name: 'login' }
+  const user = getUserFromLocalStorage()
+  return user && user.token ? undefined : { name: 'login' }
 }
 
 const redirectDashboard = () => {
-
-    let encryptedData  = localStorage.getItem("_user_data");
-    if(encryptedData){
-        let user = decrypt(encryptedData,import.meta.env.VITE_ENCRYPT_KEY)
-        if(user.data.token){
-
-            return { name: 'dashboard' }
-        }
-    }
-
+  const user = getUserFromLocalStorage()
+  return user && user.token ? { name: 'dashboard' } : undefined
 }
+
 const deleteSession = () => {
-
-    let encryptedData  = localStorage.getItem("_user_data");
-    if(encryptedData){
-        localStorage.removeItem("_user_data");
-        return { name: 'login' }
-    }
-
+  if (localStorage.getItem('_user_data')) {
+    localStorage.removeItem('_user_data')
+    return { name: 'login' }
+  }
+  return undefined
+}
+export default {
+  redirectLogin,
+  redirectDashboard,
+  deleteSession
 }
 
-export default{
-    redirectLogin,
-    redirectDashboard,
-    deleteSession
-}
+// import { decrypt } from '@/services/Encrypt'
+
+// const redirectLogin = () => {
+
+//     let encryptedData  = localStorage.getItem("_user_data");
+//     if(encryptedData){
+//         let user = decrypt(encryptedData,import.meta.env.VITE_ENCRYPT_KEY)
+
+//         if(user.token){
+//             return ;
+//         }
+
+//         return { name: 'login' }
+
+//     }
+
+//     return { name: 'login' }
+// }
+
+// const redirectDashboard = () => {
+
+//     let encryptedData  = localStorage.getItem("_user_data");
+//     if(encryptedData){
+//         let user = decrypt(encryptedData,import.meta.env.VITE_ENCRYPT_KEY)
+//         if(user.token){
+
+//             return { name: 'dashboard' }
+//         }
+//     }
+
+// }
+// const deleteSession = () => {
+
+//     let encryptedData  = localStorage.getItem("_user_data");
+//     if(encryptedData){
+//         localStorage.removeItem("_user_data");
+//         return { name: 'login' }
+//     }
+
+// }
+
+// export default{
+//     redirectLogin,
+//     redirectDashboard,
+//     deleteSession
+// }
