70dc4bc701e5b5242e5441b0d9ca1d0aadd48775
kufre-abasi <kufreabasibassey3@gmail.com>
2024-02-20 12:43:39 +0100

    feat: logos

--- src/components/Header/AuthPageNavbar.vue
+++ src/components/Header/AuthPageNavbar.vue
@@ -1,22 +1,22 @@
 <script setup>
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
-import GlobeIcon from '@/components/icons/GlobeIcon.vue'
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import GlobeIcon from "@/components/icons/GlobeIcon.vue";
 // import CaretDown from "@/components/icons/CaretDown.vue";
-const body = document.querySelector('body')
-body.classList.remove('overflow-hidden')
-const showMobile = ref(false)
-const route = useRoute()
-let dropDownItem = ref('Eng')
+const body = document.querySelector("body");
+body.classList.remove("overflow-hidden");
+const showMobile = ref(false);
+const route = useRoute();
+let dropDownItem = ref("Eng");
 const toogleMobileMenu = () => {
-  const body = document.querySelector('body')
-  showMobile.value = !showMobile.value
+  const body = document.querySelector("body");
+  showMobile.value = !showMobile.value;
   if (showMobile.value) {
-    body.classList.add('overflow-hidden')
+    body.classList.add("overflow-hidden");
   } else {
-    body.classList.remove('overflow-hidden')
+    body.classList.remove("overflow-hidden");
   }
-}
+};
 </script>
 <template>
   <div class="sticky top-0 z-30 bg-transparent">
@@ -26,14 +26,14 @@ const toogleMobileMenu = () => {
           class="lg:px-0 px-[0px] mx-auto flex items-center gap-[10px] justify-between font-bold"
         >
           <div class="flex items-center justify-between space-x-4 lg:space-x-6">
-            <router-link to="/" class="text-[30px]">
-              iSalesBook
-              <!-- <img
+            <router-link to="/" class="">
+              <!-- iSalesBook -->
+              <img
                 loading="lazy"
-                src="@/assets/BrandLogos/logo.png"
+                src="@/assets/iSalesbook.png"
                 class="h-[39px] md:h-[49.421px] w-auto"
                 alt=" logo"
-              /> -->
+              />
             </router-link>
           </div>
         </div>
