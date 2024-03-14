3428e3a6692cf9311815f100c8c48e5aba377f1f
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2024-03-14 16:52:59 +0100

    fixed all merge conflict

--- src/views/Dashboard/Product/ProductPages.vue
+++ src/views/Dashboard/Product/ProductPages.vue
@@ -40,24 +40,11 @@
       <template v-slot:default>
         <form @submit.prevent="submitForm">
           <p v-if="isError" class="text-red-500">{{ errorMessage }}</p>
-<<<<<<< HEAD
           <ReusableForm :fields="formFields"  @fetchDataForSubCategory="fetchDataForSubCategory" :isLoadingMsg="isOptionLoadingMsg" :allError="allError"/>
           <input type="submit"  v-if="!loading"  value="Submit" class="btn btn-primary mt-3">
           
                     <Loader v-else />
           
-=======
-          <ReusableForm
-            :fields="formFields"
-            @fetchDataForSubCategory="fetchDataForSubCategory"
-            :isLoadingMsg="isLoadingMsg"
-            :allError="allError"
-          />
-          <div class="flex justify-center items-center">
-            <input type="submit" v-if="!loading" value="Submit" class="btn-brand mt-3" />
-            <Loader v-else />
-          </div>
->>>>>>> main
         </form>
       </template>
     </FormModal>
@@ -74,7 +61,6 @@ import DashboardLayout from "@/components/Layouts/dashboardLayout.vue";
 import DataTableLayout from "@/components/Layouts/dataTableLayout.vue"; // read data
 import FormModal from "@/components/UI/FormModal.vue"; // show  form modal
 import ViewModal from "@/components/UI/ViewModal.vue"; // show read modal
-<<<<<<< HEAD
 import ViewModalDetail from "@/components/UI/ViewModalDetail.vue"; 
 import ReusableForm from "@/components/Form/ReusableForm.vue"  // To create form
 import Loader from "@/components/UI/Loader.vue";
@@ -83,44 +69,18 @@ import { usePostComposable} from '@/composable/usePostComposable';
 import { useSelectComposable} from '@/composable/useSelectComposable';
 import { formFields } from '@/formfields/formFields';
 
-=======
-import ViewModalDetail from "@/components/UI/ViewModalDetail.vue";
-import ReusableForm from "@/components/Form/ReusableForm.vue"; // To create form
-import apiService from "@/services/apiService";
-import Loader from "@/components/UI/Loader.vue";
-import { usePostComposable } from "@/composable/usePostComposable";
-import { formFields } from "@/formfields/formFields";
-import { useProductStore } from "@/stores/products";
-const productsStore = useProductStore();
->>>>>>> main
 
 const formTitle = "Add Product";
 const modalTitle = "View Product";
 const router = useRouter();
 const url = '/all-product-sub-categories-by-category-id';
 const products = ref();
-<<<<<<< HEAD
 const { showModal, showViewModal,loading, allError,forceUpdate,errorMessage,isError,closeModal,closeViewModal,submitForm}
        = usePostComposable('/products', formFields);
 
       // fetchDataForSubCategory is emitted
 const { fetchDataForSelect, fetchDataForSubCategory,isOptionLoadingMsg} = useSelectComposable(formFields, url,"Category", "Sub Category", "sub_category_name"); 
 
-=======
-const {
-  showModal,
-  showViewModal,
-  isLoadingMsg,
-  loading,
-  allError,
-  forceUpdate,
-  errorMessage,
-  isError,
-  closeModal,
-  closeViewModal,
-  submitForm,
-} = usePostComposable("/products", formFields);
->>>>>>> main
 
 const openProductDetailModal = (product) => {
   // console.log(product);
@@ -141,73 +101,10 @@ const handleDelete = (product) => {
   console.log("Deleting product", product);
 };
 
-<<<<<<< HEAD
-=======
-const fetchDataForSubCategory = async (value, label) => {
-  if (label === "Category") {
-    isLoadingMsg.value = "Please wait";
-    try {
-      const response = await apiService.get(
-        `/all-product-sub-categories-by-category-id/${value}`
-      );
-      //locate the sub category form field
-      const subCategoryField = formFields.value.find(
-        (field) => field.label === "Sub Category"
-      );
-      if (subCategoryField) {
-        if (response.length === 0) {
-          // If response is empty, set options for "Sub Category" dropdown to include just "No Subcategory"
-          subCategoryField.options = [{ value: "", label: "No Subcategory" }];
-        } else {
-          // If response is not empty, map response data to options for "Sub Category" dropdown
-          subCategoryField.options = [
-            { value: "", label: "Select an option", disabled: true },
-            ...response.map((item) => ({
-              value: item.id,
-              label: item.sub_category_name,
-            })),
-          ];
-        }
-        isLoadingMsg.value = "";
-      } else {
-        console.error("Sub Category field not found.");
-        isLoadingMsg.value = "Some went wrong. Try again";
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      isLoadingMsg.value = "Some went wrong. Try again";
-    }
-  }
-};
-
-const fetchDataForSelect = async (field, endpoint, valueProp, labelProp) => {
-  try {
-    const response = await apiService.get(endpoint);
-    const fieldObject = formFields.value.find((f) => f.label === field);
-    if (fieldObject) {
-      fieldObject.options = [
-        { value: "", label: "Select an option", disabled: true },
-        ...response.map((item) => ({ value: item[valueProp], label: item[labelProp] })),
-      ];
-    } else {
-      console.error(`Field with label '${field}' not found.`);
-    }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-
->>>>>>> main
 // Fetch data for select options on component mount
 onMounted(async () => {
   await fetchDataForSelect("Measurement", "/measurements", "id", "measurement_name");
   await fetchDataForSelect("Category", "/product-categories", "id", "category_name");
 });
-onMounted(async () => {
-  try {
-    await productsStore.handleGetProducts(productsStore?.products?.value?.current_page);
-  } catch (error) {
-    console.error(error);
-  }
-});
+
 </script>
