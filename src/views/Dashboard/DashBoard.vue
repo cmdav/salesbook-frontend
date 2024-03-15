a1a73baa749c1f902a5f3d3839f5e21bf5f966fb
kufre-abasi <kufreabasibassey3@gmail.com>
2024-03-15 11:04:00 +0100

    feat: chart

--- src/views/Dashboard/DashBoard.vue
+++ src/views/Dashboard/DashBoard.vue
@@ -116,75 +116,14 @@
             </div>
           </div>
           <div class="chart hidden bg-white rounded-[8px] min-h-[100vh] p-4"></div>
-          <div class="bg-white py-6 mt-12 rounded-lg">
+          <div class="bg-white border-secondary-400 border-[1px] py-6 mt-12 rounded-lg">
             <div class="flex lg:flex-row flex-col gap-3 px-4 justify-between mb-4">
               <div class="flex lg:flex-row flex-col justify-between w-full gap-3">
-                <div class="flex lg:flex-row flex-col gap-3">
-                  <!-- <select
-                    class="border-[1px] border-gray-200 px-6 py-[12px] bg-[#f6f6f6] rounded-[4px]"
-                    name=""
-                    id=""
-                  >
-                    <option>All Status</option>
-                    <option value="ACTIVE">Active</option>
-                    <option value=""></option>
-                    <option value=""></option></select
-                  ><input
-                    class="border-[1px] border-gray-200 outline-none bg-[#F6F8FB] px-4 py-[12px] rounded-[4px]"
-                    type="search"
-                    placeholder="Search"
-                    name=""
-                    id=""
-                  /> -->
-                </div>
-
-                <button
-                  v-if="feature.includes('INVENTORY')"
-                  class="p-4 bg-brand py-[12px] text-white rounded-[4px]"
-                >
-                  Add Inventory
-                </button>
+                <div class="flex lg:flex-row flex-col gap-3"><h4>Sales Table</h4></div>
               </div>
             </div>
             <div class="overflow-x-scroll hide-scrollbar">
-              <div class="table-container overflow-x-scroll">
-                <table class="table-auto w-full">
-                  <thead class="bg-[#F9FBFF] text-[#A8AABC] text-[14px]">
-                    <tr>
-                      <th class="py-4 pl-4 flex">
-                        <input
-                          class="mr-2 outline-none"
-                          type="checkbox"
-                          name=""
-                          id=""
-                        />S/N
-                      </th>
-                      <th class="text-left p-4 pr-0 px-6 capitalize">name</th>
-                      <th class="text-left p-4 pr-0 px-6 capitalize">Product</th>
-                      <th class="text-left p-4 pr-0 px-6 capitalize">Quantity</th>
-                      <th class="text-left p-4 pr-0 px-6 capitalize">Amount</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <!-- <tr class="border-b text-[14px]">
-                      <td class="py-4 pl-4">
-                        <input class="mr-2 outline-none" type="checkbox" value="1" />1
-                      </td>
-                      <td class="text-left p-4 pr-0 pl-6 capitalize">
-                        <a href="/customers/manage-customer/john-doe" class=""
-                          >John Doe</a
-                        >
-                      </td>
-                      <td class="text-left p-4 pr-0 pl-6 capitalize">Basic</td>
-                      <td class="text-left p-4 pr-0 pl-6 capitalize">$1000</td>
-                      <td class="text-left p-4 pr-0 pl-6 capitalize">$1000</td>
-                      <td class="text-left p-4 pr-0 pl-6 capitalize">$1000</td>
-                      <td class="text-left p-4 pr-0 pl-6 capitalize">Basic</td>
-                    </tr> -->
-                  </tbody>
-                  <!---->
-                </table>
-              </div>
+              <ChartComponent />
             </div>
           </div>
         </div>
@@ -194,7 +133,7 @@
 </template>
 <script setup>
 import { onMounted } from "vue";
-
+import ChartComponent from "@/components/UI/Chart/ChartComponent.vue";
 import { useStore } from "@/stores/user";
 import DashboardLayout from "@/components/Layouts/dashboardLayout.vue";
 import { useSupplierStore } from "@/stores/suppliers";
