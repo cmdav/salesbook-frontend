90bc030124ccc171c0d3cd43598ec2b863909d59
radiant-O <bukoyeolaniyi@gmail.com>
2024-05-06 18:33:19 +0100

    updated

--- src/views/Dashboard/SupplierProduct/SupplierProductPage.vue
+++ src/views/Dashboard/SupplierProduct/SupplierProductPage.vue
@@ -77,10 +77,37 @@
   endpoint="price-notifications"
   hideToggleButtonLabel: false
   :excludedKeys="['id','product_type_id','supplier_id','product_category_name']"
-  :additionalColumns="[{ name: 'Accept', action: acceptProduct , disabled: acceptDisabled  }, { name: 'Decline', action: declineProduct, disabled: declineDisabled }]"
+  :additionalColumns="[{ name: 'Accept', action: (product) => handleAcceptModal(product), disabled: acceptDisabled  }, { name: 'Decline', action: declineProduct, disabled: declineDisabled }]"
   >
 </SupplierTableLayout>
 </div>
+
+<SaleFormModal
+      v-if="showModal"
+      :buttonLable="'Accept & Update Price'"
+      :loading="loading"
+      @close="handleAcceptModal"
+      @submitForm="acceptProduct"
+      title="Add Price"
+    >
+      <div class="my-8 flex flex-col gap-2">
+        <div class="overflow-y-auto flex flex-col gap-2 max-h-[340px]">
+          <div class="flex flex-col gap-2">
+            <div class="flex flex-col gap-2">
+              <div class="w-full">
+                <label class="block text-sm font-medium text-gray-700">Add Selling Price </label>
+                <input
+                placeholder="Enter Selling Price"
+                  v-model="formData.sellingPrice"
+                  type="text"
+                  class="w-full font-light font-Satoshi400 border-neutral-900 text-[14px] outline-none !p-[14px] border-[1px] opacity-[0.8029] rounded-[4px] text-sm"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SaleFormModal>
   </DashboardLayout>
 </template>
 <script setup>
@@ -88,17 +115,14 @@
 import DashboardLayout from "@/components/Layouts/dashboardLayout.vue";
 import { onMounted, ref, reactive, computed } from "vue";
   import { useSupplierStore } from "@/stores/suppliers";
-// import SaleFormModal from "@/components/UI/Modal/SalesFormModal.vue";
+import SaleFormModal from "@/components/UI/Modal/SalesFormModal.vue";
   const supplierStore = useSupplierStore();
   //handles all component import
   import { useSharedComponent } from "@/composable/useSharedComponent";
   const {
-    SupplierDashboardLayout,
     SupplierTableLayout,
-    DataTableLayout,
     usePostComposable,
     defineEmits,
-    useStore
   } = useSharedComponent('supplier-home-page');
 
   const emit = defineEmits("forceRefresh");
@@ -108,25 +132,37 @@ import { onMounted, ref, reactive, computed } from "vue";
 
   const showModal = ref(false);
 
-  let loading = ref(false);
+  const formData = ref({
+    sellingPrice: "",
+    productId: ""
+});
 
-  const lastClicked = ref(null); 
+let loading = ref(false);
+
+const lastClicked = ref(null); 
 const acceptDisabled = ref(false); 
 const declineDisabled = ref(false); 
 
 
- const acceptProduct = async (product) => {
-  await handlePriceStatus(product.id, { status: "accepted", selling_price: product.selling_price });
-  lastClicked.value = "accept"; 
-  declineDisabled.value = true; 
-  acceptDisabled.value = false; 
+ const handleAcceptModal = async (product) => {
+  showModal.value = !showModal.value;
+  if (product) {
+    formData.value.productId = product.id;
+  }
 };
 
 const declineProduct = async (product) => {
-  await handlePriceStatus(product.id, { status: "decline", selling_price: product.selling_price });
+  await handlePriceStatus(product.id, { status: "decline" });
+
   lastClicked.value = "decline"; 
   declineDisabled.value = false; 
   acceptDisabled.value = true; 
+};
+
+const acceptProduct = async () => {
+  await handlePriceStatus(formData.value.productId, { status: "accepted", selling_price: formData.value.sellingPrice });
+  await handleAcceptModal()
+
 };
 
 const forceRefresh = () => {
