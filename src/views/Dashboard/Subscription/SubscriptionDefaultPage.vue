4ce134d259bf5fe3f7d0674d6dd6b6d78a4f358c
trini7y <okekedesmond71@gmail.com>
2024-06-01 20:33:08 +0100

    Added the subscription page

--- src/views/Dashboard/Subscription/SubscriptionDefaultPage.vue
+++ src/views/Dashboard/Subscription/SubscriptionDefaultPage.vue
@@ -2,13 +2,13 @@
     <DashboardLayout pageTitle="Settings Page">
         <div class="container p-0 lg:p-6 lg:py-3 py-4 mb-5">
             <Tab>
-                <template #tab1> Subscribers </template>
-                <template #tab2> Subsciption Plan </template>
+                <!-- <template #tab1> Subscribers </template> -->
+                <template #tab1> Subsciption Plan </template>
 
-                <template #view1>
+                <!-- <template #view1>
                     <SubscribersPage />
-                </template>
-                <template #view2>
+                </template> -->
+                <template #view1>
                     <SubscribersPlan />
                 </template>
             </Tab>
