5c1cbe301e8447096f320726f4a08f25f8e06876
radiant-o <bukoyeolaniyi@gmail.com>
2024-07-19 12:59:20 +0100

    updated

--- src/components/Auth/Forms/Individual.vue
+++ src/components/Auth/Forms/Individual.vue
@@ -3,47 +3,33 @@
     <div class="flex flex-col gap-[17px]">
       <div class="flex lg:flex-row flex-col w-full gap-[20px]">
         <div class="mb-3 flex flex-col w-full">
-          <AuthInput
-            label="First Name*"
-            :error="errors.firstName"
-            :errorsMsg="errorsMsg.firstName"
-            type="text"
-            placeholder="Enter first name"
-            v-model="formData.firstName"
-          />
+          <AuthInput label="First Name*" :error="errors.firstName" :errorsMsg="errorsMsg.firstName" type="text"
+            placeholder="Enter first name" v-model="formData.firstName" />
         </div>
         <div class="mb-3 flex flex-col w-full">
-          <AuthInput
-            label="Last Name*"
-            :error="errors.lastName"
-            type="text"
-            placeholder="Enter last name"
-            :errorsMsg="errorsMsg.lastName"
-            v-model="formData.lastName"
-          />
+          <AuthInput label="Last Name*" :error="errors.lastName" type="text" placeholder="Enter last name"
+            :errorsMsg="errorsMsg.lastName" v-model="formData.lastName" />
         </div>
       </div>
 
       <div class="flex lg:flex-row flex-col w-full gap-[20px]">
         <div class="mb-3 flex flex-col w-full">
-          <AuthInput
-            label="Company Name*"
-            :error="errors.company_name"
-            :errorsMsg="errors.company_name"
-            type="text"
-            placeholder="Enter company name"
-            v-model="formData.company_name"
-          />
+          <AuthInput label="Company Name*" :error="errors.company_name" :errorsMsg="errors.company_name" type="text"
+            placeholder="Enter company name" v-model="formData.company_name" />
         </div>
         <div class="mb-3 flex flex-col w-full">
-          <AuthInput
-            label="Email Address*"
-            :error="errors.email"
-            :errorsMsg="errorsMsg.email"
-            type="email"
-            placeholder="Enter email address"
-            v-model="formData.email"
-          />
+          <AuthInput label="Email Address*" :error="errors.email" :errorsMsg="errorsMsg.email" type="email"
+            placeholder="Enter email address" v-model="formData.email" />
+        </div>
+      </div>
+      <div class="flex lg:flex-row flex-col w-full gap-[20px]">
+        <div class="mb-3 flex flex-col w-full">
+          <AuthInput label="Date of Birth (Optional)"  type="text" placeholder="Enter Address"
+            v-model="dob" :max="minDate" :errorsMsg="dobError" />
+        </div>
+        <div class="mb-3 flex flex-col w-full">
+          <AuthInput label="Phone number*" :error="false" type="tel" placeholder="Enter Phone number"
+            v-model="phoneNo" />
         </div>
       </div>
       <div class="flex lg:flex-row flex-col w-full gap-[20px]">
@@ -58,100 +44,55 @@
             :errorsMsg="errorsMsg.address"
           />
         </div>
-        <div class="mb-3 flex flex-col w-full">
-          <AuthInput
-            label="Phone number*"
-            :error="false"
-            type="tel"
-            placeholder="Enter Phone number"
-            v-model="phoneNo"
-          />
-        </div>
       </div>
-      <!-- <div class="flex lg:flex-row flex-col w-full gap-[20px]">
-        <div class="mb-3 flex flex-col w-full">
-          <AuthInput
-            label="Phone number*"
-            :error="false"
-            type="tel"
-            placeholder="Enter Phone number"
-            v-model="phoneNo"
-          />
-        </div>
-      </div> -->
+
       <div class="flex lg:flex-row flex-col w-full gap-[20px]">
+
         <div class="mb-3 flex flex-col w-full">
-          <PasswordInput
-            label="Password*"
-            :error="errors.password"
-            :errorsMsg="errorsMsg.password"
-            v-model="formData.password"
-            placeholder="**********"
-          />
+          <PasswordInput label="Password*" :error="errors.password" :errorsMsg="errorsMsg.password"
+            v-model="formData.password" placeholder="**********" />
         </div>
         <div class="mb-3 flex flex-col w-full">
-          <PasswordInput
-            label="Confirm Password"
-            :error="errors.confirmPassword || !passwordsMatch"
-            :errorsMsg="errorsMsg.confirmPassword"
-            placeholder="Confirm Password*"
-            v-model="formData.confirmPassword"
-          />
+          <PasswordInput label="Confirm Password" :error="errors.confirmPassword || !passwordsMatch"
+            :errorsMsg="errorsMsg.confirmPassword" placeholder="Confirm Password*" v-model="formData.confirmPassword" />
         </div>
       </div>
       <!-- password reg dropdown -->
-      <div
-        v-if="formData.password.length && !isValidPassword"
-        class="grid grid-cols-3 font-Satoshi400 text-gray-400 text-[12px] gap-2"
-      >
+      <div v-if="formData.password.length && !isValidPassword"
+        class="grid grid-cols-3 font-Satoshi400 text-gray-400 text-[12px] gap-2">
         <div class="flex items-center gap-2">
-          <div
-            :class="{
+          <div :class="{
               ' bg-[#55eb37]': isMinLengthMet,
               ' bg-gray-200': !isMinLengthMet,
-            }"
-            class="h-3 w-3 flex justify-center items-center rounded-full"
-          ></div>
+            }" class="h-3 w-3 flex justify-center items-center rounded-full"></div>
           <p>A minimum of 8 characters</p>
         </div>
         <div class="flex items-center gap-2">
-          <div
-            :class="{
+          <div :class="{
               ' bg-[#55eb37]': isAtLeastOneNumber,
               ' bg-gray-200': !isAtLeastOneNumber,
-            }"
-            class="h-3 w-3 flex justify-center items-center rounded-full"
-          ></div>
+            }" class="h-3 w-3 flex justify-center items-center rounded-full"></div>
           <p>Atleast one number</p>
         </div>
         <div class="flex items-center gap-2">
-          <div
-            :class="{
+          <div :class="{
               ' bg-[#55eb37]': isAtLeastOneSpecialChar,
               ' bg-gray-200': !isAtLeastOneSpecialChar,
-            }"
-            class="h-3 w-3 flex justify-center items-center rounded-full"
-          ></div>
+            }" class="h-3 w-3 flex justify-center items-center rounded-full"></div>
           <p>Atleast one special character</p>
         </div>
         <div class="flex items-center gap-2">
-          <div
-            :class="{
+          <div :class="{
               ' bg-[#55eb37]': isAtLeastOneLowercase,
               ' bg-gray-200': !isAtLeastOneLowercase,
-            }"
-            class="h-3 w-3 flex justify-center items-center rounded-full"
-          ></div>
+            }" class="h-3 w-3 flex justify-center items-center rounded-full"></div>
           <p>Atleast one lowercase letter</p>
         </div>
         <div class="flex items-center gap-2">
-          <div
-            :class="{
+          <div :class="{
               ' bg-[#55eb37]': isAtLeastOneUppercase,
               ' bg-gray-200': !isAtLeastOneUppercase,
-            }"
-            class="h-3 w-3 flex justify-center items-center rounded-full"
-          ></div>
+            }" class="h-3 w-3 flex justify-center items-center rounded-full"></div>
           <p>Atleast one uppercase letter</p>
         </div>
       </div>
@@ -159,34 +100,24 @@
     <div class="w-full flex justify-between">
       <p class="text-[14px]">
         Already have an account
-        <router-link to="/login" class="font-medium text-brand text-[14px]"
-          >Login</router-link
-        >
+        <router-link to="/login" class="font-medium text-brand text-[14px]">Login</router-link>
       </p>
     </div>
 
     <div class="flex flex-col lg:flex-row w-full gap-[30px] mt-4">
-      <button
-        :disabled="loading"
-        @click="handleSignup()"
+      <button :disabled="loading" @click="handleSignup()"
         :class="!isFormValid ? '!bg-primary-100/[30%] cursor-not-allowed' : 'bg-brand'"
-        class="btn-brand !rounded-[5px] flex gap-2 items-center justify-center !text-white text-[14px] !py-[16px] font-semibold w-full"
-      >
+        class="btn-brand !rounded-[5px] flex gap-2 items-center justify-center !text-white text-[14px] !py-[16px] font-semibold w-full">
         <span v-if="!loading" class="font-semibold !text-[15px]">Submit</span>
         <Loader v-else />
       </button>
     </div>
 
     <div class="flex items-center justify-center mb-10 text-center gap-[4px]">
-      <span class="font-normal text-[14px]"
-        >By continuing you agree to the
-        <router-link class="text-[#007BFF] font-medium" to="/"
-          >terms of service</router-link
-        >
+      <span class="font-normal text-[14px]">By continuing you agree to the
+        <router-link class="text-[#007BFF] font-medium" to="/">terms of service</router-link>
         and
-        <router-link class="text-[#007BFF] font-medium" to="/"
-          >privacy policy</router-link
-        >
+        <router-link class="text-[#007BFF] font-medium" to="/">privacy policy</router-link>
       </span>
     </div>
   </div>
@@ -228,7 +159,7 @@ const errors = reactive({
   confirmPassword: false,
 });
 const dobError = ref(false);
-// const dobErrorMsg = ref("date of birth is lower than 18");
+const dobErrorMsg = ref("date of birth is lower than 18");
 const confirmPassword = ref("");
 
 const errorsMsg = {
@@ -343,7 +274,7 @@ const isFormValid = computed(() => {
     formData.email.trim() !== "" &&
     formData.password.trim() !== "" &&
     formData.company_name.trim() !== "" &&
-    // formData.code.trim() !== "" &&
+    formData.company_address !== "" &&
     confirmPassword.value.trim() !== ""
   );
 });
@@ -353,6 +284,7 @@ const clearInputs = () => {
     (formData.email = ""),
     (formData.password = "");
     (formData.company_name = "");
+    (formData.company_address = "");
   middelName.value = "";
   phoneNo.value = "";
   // formData.code = "";
@@ -377,8 +309,8 @@ const handleSignup = async () => {
     company_name: formData.company_name,
     middle_name: middelName.value,
     phone_number: phoneNo.value,
-    address: formData.address,
-    // dob: dob.value,
+    company_address: formData.ada,
+    dob: dob.value,
     email: formData.email,
     password: formData.password,
     password_confirmation: formData.confirmPassword,
