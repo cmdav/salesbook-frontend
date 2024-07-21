2b775f1f8fc9679e4bf6bdf6ee7e6bd4e73279a5
radiant-o <bukoyeolaniyi@gmail.com>
2024-07-21 22:30:44 +0100

    update receipt

--- src/views/Dashboard/Product/Sale/ViewSalePage.vue
+++ src/views/Dashboard/Product/Sale/ViewSalePage.vue
@@ -118,18 +118,6 @@ function handleBranchChange(selectedBranchId) {
 }
 
 
-// async function fetchBranch(branchId = 1) {
-//   try {
-//     const response = await apiService.get(`sales?branch_id=${branchId}`);
-//     data.value = response;
-//     console.log(response)
-    
-//     return data.value;
-//   } catch (error) {
-//     console.error('Failed to fetch sales data:', error);
-//   }
-// }
-
 async function fetchBranch(branchId) {
   try {
     const response = await apiService.get(`/sales?branch_id=${branchId}`);
@@ -225,7 +213,6 @@ const formatNumber = (amount) => {
   }).format(amount);
 };
 
-
 const generateReceiptPDF = (receiptData, userProfileDetails) => {
   console.log("Hello Receipt")
   const doc = new jsPDF();
@@ -238,40 +225,39 @@ const generateReceiptPDF = (receiptData, userProfileDetails) => {
 
   doc.setFont(headerStyle.fontStyle, 'normal');
   doc.setFontSize(headerStyle.fontSize);
-  doc.text(`${userProfileDetails?.company_name}`, 105, 20, null, null, 'center');
-
+  doc.text(`${userProfileDetails?.company_name || ''}`, 105, 20, null, null, 'center');
 
   doc.setFont(companyDetailsStyle.fontStyle, 'normal');
   doc.setFontSize(companyDetailsStyle.fontSize);
-  doc.text(`Address ${userProfileDetails?.company_address}`, 105, 28, null, null, 'center');
-  doc.text(`Email: ${userProfileDetails?.email}`, 105, 36, null, null, 'center');
-  doc.text(`Phone No: ${userProfileDetails?.phone_number}`, 105, 44, null, null, 'center');
+  doc.text(`Address: ${userProfileDetails?.company_address || ''}`, 105, 28, null, null, 'center');
+  doc.text(`Email: ${userProfileDetails?.email || ''}`, 105, 36, null, null, 'center');
+  doc.text(`Phone No: ${userProfileDetails?.phone_number || ''}`, 105, 44, null, null, 'center');
 
   doc.setFont(invoiceStyle.fontStyle, 'normal');
   doc.setFontSize(invoiceStyle.fontSize);
-  doc.text('RECEIPT', 105, 55, null, null, 'center'); 
+  doc.text('RECEIPT', 105, 55, null, null, 'center');
 
   doc.setFont(sectionHeaderStyle.fontStyle, 'normal');
   doc.setFontSize(sectionHeaderStyle.fontSize);
-  doc.text(`Branch Name: ${receiptData.transaction_details.branch_name}`, 20, 70);
-  doc.text(`Branch Address: ${receiptData.transaction_details.branch_city} ${receiptData.transaction_details.branch_state}`, 20, 78);
-  doc.text(`Branch Phone Number: ${receiptData.transaction_details.branch_phone_number}`, 20, 86);
-  doc.text(`Customer Name: ${receiptData.transaction_details.customer_detail}`, 20, 102);
-  doc.text(`Customer PhoneNum: ${receiptData.transaction_details.customer_phone_number}`, 20, 112);
-  doc.text(`Transaction ID: ${receiptData.transaction_details.transaction_id}`, 20, 122);
-  doc.text(`Payment Method: ${receiptData.transaction_details.payment_method}`, 20, 132);
-  doc.text(`Date: ${receiptData.transaction_details.created_at}`, 20, 142);
+  doc.text(`Branch Name: ${receiptData.transaction_details.branch_name || ''}`, 20, 70);
+  doc.text(`Branch Address: ${receiptData.transaction_details.branch_city || ''} ${receiptData.transaction_details.branch_state || ''}`, 20, 78);
+  doc.text(`Branch Phone Number: ${receiptData.transaction_details.branch_phone_number || ''}`, 20, 86);
+  doc.text(`Customer Name: ${receiptData.transaction_details.customer_detail || ''}`, 20, 102);
+  doc.text(`Customer PhoneNum: ${receiptData.transaction_details.customer_phone_number || ''}`, 20, 112);
+  doc.text(`Transaction ID: ${receiptData.transaction_details.transaction_id || ''}`, 20, 122);
+  doc.text(`Payment Method: ${receiptData.transaction_details.payment_method || ''}`, 20, 132);
+  doc.text(`Date: ${receiptData.transaction_details.created_at || ''}`, 20, 142);
 
   const tableColumn = ["Product Name", "Price(NGN)", "Quantity", "VAT(NGN)", "Total Price(NGN)"];
   const tableRows = [];
 
   receiptData.items.forEach((item) => {
     const itemData = [
-      item.product_type_name,
-      item.price_sold_at,
-      item.quantity,
-      item.vat.toFixed(2),
-      `${item.total_price}`
+      item.product_type_name || '',
+      item.price_sold_at || '',
+      item.quantity || '',
+      item.vat !== null ? item.vat.toFixed(2) : '',
+      `${item.total_price || ''}`
     ];
     tableRows.push(itemData);
   });
@@ -280,18 +266,21 @@ const generateReceiptPDF = (receiptData, userProfileDetails) => {
     head: [tableColumn],
     body: tableRows,
     startY: 152,
-    styles: { fontSize: itemStyle.fontSize },
-    theme: 'plain',
-    tableLineColor: [0, 0, 0], 
-    tableLineWidth: 0.1, 
+    styles: {
+      fontSize: itemStyle.fontSize,
+      lineColor: [0, 0, 0], // Black border
+      lineWidth: 0.1,
+      fillColor: [255, 255, 255], // White background
+      textColor: [0, 0, 0] // Black text color
+    },
+    theme: 'grid', // Ensure all cells have borders
   });
 
   let finalY = doc.autoTable.previous.finalY + 10;
   doc.setFont(invoiceStyle.fontStyle)
   doc.setFontSize(invoiceStyle.fontSize);
-  doc.text(`Total Amount (NGN): ${formatNumber(receiptData.transaction_details.transaction_amount.toFixed(2))}`, doc.internal.pageSize.width - 20, finalY, null, null, 'right');
+  doc.text(`Total Amount (NGN): ${formatNumber(receiptData.transaction_details.transaction_amount?.toFixed(2) || '0.00')}`, doc.internal.pageSize.width - 20, finalY, null, null, 'right');
 
-   
   doc.setFontSize(itemStyle.fontSize);
   doc.text('Thanks for your patronage!', 105, finalY + 15, null, null, 'center');
 
