6b5e201ef39e2118895cc87e0c084c593f43f16c
radiant-o <bukoyeolaniyi@gmail.com>
2024-08-05 13:54:57 +0100

    updated

--- src/views/Dashboard/Product/ProductType/CreateProductType.vue
+++ src/views/Dashboard/Product/ProductType/CreateProductType.vue
@@ -1,130 +1,161 @@
 <template>
   <DashboardLayout pageTitle="Create Product Type">
+    <div class="container p-0 lg:p-6 lg:py-3 py-4 mb-5">
+      <!-- Navigation buttons at the top -->
+      <div class="top-buttons">
+        <router-link to="/product-type" class="button back-btn">Back</router-link>
+      </div>
 
-      <div class="container p-0 lg:p-6 lg:py-3 py-4 mb-5">
-    <!-- Navigation buttons at the top -->
-    <div class="top-buttons">
-      <router-link to="/product-type" class="button back-btn">Back</router-link>
+      <!-- Form for adding a new sale -->
+      <form @submit.prevent="handleSubmit">
+        <div class="input-group w-[70%]">
+          <label class="block text-sm font-medium text-gray-700">Product</label>
+          <select v-model="formState.product" class="select-input" required>
+            <option selected>Select Product...</option>
+            <option v-for="product in products" :key="product.id" :value="product.id">
+              {{ product.product_name }}
+            </option>
+          </select>
+        </div>
+
+        <div class="input-group w-[70%]">
+          <label class="block text-sm font-medium text-gray-700">Product Type Name</label>
+          <input
+            v-model="formState.productTypeName"
+            required
+            type="text"
+            class="input"
+            placeholder="Enter Product Type Name"
+          />
+        </div>
+
+        <div class="input-group w-[70%]">
+          <label class="block text-sm font-medium text-gray-700">Product Type Image</label>
+          <input ref="fileInput" type="file" @change="handleImageChange" />
+          <img
+            v-if="newImage"
+            :src="newImage"
+            class="mb-4 h-20 w-30 rounded-md object-cover"
+            alt="Current Image"
+          />
+        </div>
+
+        <div class="input-group w-[70%]">
+          <label class="block text-sm font-medium text-gray-700">Product Type Description</label>
+          <input
+            v-model="formState.productTypeDescription"
+            required
+            type="text"
+            placeholder="Enter Product Type Description"
+            class="input"
+          />
+        </div>
+
+        <div class="input-group w-[70%]">
+          <label class="block text-sm font-medium text-gray-700">Barcode</label>
+          <input v-model="formState.barcode" type="password" class="input" />
+        </div>
+
+        <div class="input-group w-[70%]">
+          <label class="block text-sm font-medium text-gray-700">Measurement</label>
+          <select v-model="formState.measurement" class="select-input" required>
+            <option selected>Select Measurement...</option>
+            <option
+              v-for="measurement in measurements"
+              :key="measurement.id"
+              :value="measurement.id"
+            >
+              {{ measurement.measurement_name }}
+            </option>
+          </select>
+        </div>
+
+        <div class="input-group w-full">
+          <label class="block text-sm font-medium text-gray-700">Container Type</label>
+          <div class="flex">
+            <div class="w-[70%]">
+              <select
+                v-model="selectedContainerType"
+                class="select-input"
+                @change="fetchContainerTypeCapacities"
+                required
+              >
+                <option selected>Select Container Type...</option>
+                <option v-for="type in containerTypes" :key="type.id" :value="type.id">
+                  {{ type.container_type_name }}
+                </option>
+              </select>
+            </div>
+            <button type="button" class="button btn-brand ml-4" @click="addContainerType">
+              Add Container Type
+            </button>
+          </div>
+        </div>
+
+        <div class="input-group w-full">
+          <label class="block text-sm font-medium text-gray-700">Container Type Capacity</label>
+          <div class="flex">
+            <div class="w-[70%]">
+              <select v-model="formState.containerTypeCapacity" class="select-input" required>
+                <option selected>Select Container Capacity...</option>
+                <option
+                  v-for="capacity in containerTypeCapacities"
+                  :key="capacity.id"
+                  :value="capacity.id"
+                >
+                  {{ capacity.container_capacity }}
+                </option>
+              </select>
+            </div>
+            <button type="button" class="button btn-brand ml-4" @click="addContainerCapacity">
+              Add Container Capacity
+            </button>
+          </div>
+        </div>
+
+        <!-- Submit button for the form -->
+        <button type="submit" class="button submit-button" :disabled="isSubmitting">
+          {{ isSubmitting ? 'Submitting...' : 'Submit' }}
+        </button>
+      </form>
     </div>
-
-    <!-- Form for adding a new sale -->
-    <form @submit.prevent="handleSubmit">
-      <div class="input-group w-[70%]">
-        <label class="block text-sm font-medium text-gray-700">Product</label>
-        <select v-model="formState.product" class="select-input" required>
-          <option selected>Select Product...</option>
-          <option v-for="product in products" :key="product.id" :value="product.id">
-            {{ product.product_name }}
-          </option>
-        </select>
-      </div>
-
-      <div class="input-group w-[70%]">
-        <label class="block text-sm font-medium text-gray-700">Product Type Name</label>
-        <input v-model="formState.productTypeName" required type="text" class="input" placeholder="Enter Product Type Name" />
-      </div>
-
-      <div class="input-group w-[70%]">
-        <label class="block text-sm font-medium text-gray-700">Product Type Image</label>
-        <input ref="fileInput" type="file" @change="handleImageChange" />
-        <img v-if="newImage" :src="newImage" class="mb-4 h-20 w-30 rounded-md object-cover" alt="Current Image" />
-      </div>
-
-      <div class="input-group w-[70%]">
-        <label class="block text-sm font-medium text-gray-700">Product Type Description</label>
-        <input v-model="formState.productTypeDescription" required type="text" placeholder="Enter Product Type Description" class="input" />
-      </div>
-
-      <div class="input-group w-[70%]">
-        <label class="block text-sm font-medium text-gray-700">Barcode</label>
-        <input v-model="formState.barcode" type="password" class="input" />
-      </div>
-
-      <div class="input-group w-[70%]">
-        <label class="block text-sm font-medium text-gray-700">Measurement</label>
-        <select v-model="formState.measurement" class="select-input" required>
-          <option selected>Select Measurement...</option>
-          <option v-for="measurement in measurements" :key="measurement.id" :value="measurement.id">
-            {{ measurement.measurement_name }}
-          </option>
-        </select>
-      </div>
-
-      <div class="input-group w-full">
-        <label class="block text-sm font-medium text-gray-700">Container Type</label>
-        <div class="flex">
-          <div class="w-[70%]">
-            <select v-model="selectedContainerType" class="select-input" @change="fetchContainerTypeCapacities" required>
-              <option selected>Select Container Type...</option>
-              <option v-for="type in containerTypes" :key="type.id" :value="type.id">
-                {{ type.container_type_name }}
-              </option>
-            </select>
-          </div>
-          <button type="button" class="button btn-brand ml-4" @click="addContainerType">
-            Add Container Type
-          </button>
-        </div>
-      </div>
-
-      <div class="input-group w-full">
-        <label class="block text-sm font-medium text-gray-700">Container Type Capacity</label>
-        <div class="flex">
-          <div class="w-[70%]">
-            <select v-model="formState.containerTypeCapacity" class="select-input" required>
-              <option selected>Select Container Capacity...</option>
-              <option v-for="capacity in containerTypeCapacities" :key="capacity.id" :value="capacity.id">
-                {{ capacity.container_capacity }}
-              </option>
-            </select>
-          </div>
-          <button type="button" class="button btn-brand ml-4" @click="addContainerCapacity">
-            Add Container Capacity
-          </button>
-        </div>
-      </div>
-
-      <!-- Submit button for the form -->
-      <button type="submit" class="button submit-button" :disabled="isSubmitting">
-        {{ isSubmitting ? 'Submitting...' : 'Submit' }}
-      </button>
-    </form>
-  </div>
 
     <!-- Modal for Container Type and Container Capacity -->
     <ContainerTypeModal v-if="showModal" @close="closeModal" />
-    <ContainerTypeCapacitiesModal v-if="displayModal" :containerTypeId="selectedContainerType" @close="closeModal" />
+    <ContainerTypeCapacitiesModal
+      v-if="displayModal"
+      :containerTypeId="selectedContainerType"
+      @close="closeModal"
+    />
   </DashboardLayout>
 </template>
 
-
-
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
-import apiService from '@/services/apiService';
-import { catchAxiosError, catchAxiosSuccess } from '@/services/Response';
-import ContainerTypeModal from '@/components/UI/Modal/containerTypeModal.vue';
+import { ref, reactive, onMounted } from 'vue'
+import apiService from '@/services/apiService'
+import { catchAxiosError, catchAxiosSuccess } from '@/services/Response'
+import ContainerTypeModal from '@/components/UI/Modal/containerTypeModal.vue'
 
-import ContainerTypeCapacitiesModal from '@/components/UI/Modal/containerCapacitiesModal.vue';
-import { useRouter } from 'vue-router';
+import ContainerTypeCapacitiesModal from '@/components/UI/Modal/containerCapacitiesModal.vue'
+import { useRouter } from 'vue-router'
 
-const router = useRouter();
-const fileInput = ref(null);
-const newImage = ref(null);
-const isSubmitting = ref(false);
+const router = useRouter()
+const fileInput = ref(null)
+const newImage = ref(null)
+const isSubmitting = ref(false)
 
-const showModal = ref(false);
+const showModal = ref(false)
 const displayModal = ref(false)
 
 const addContainerCapacity = () => {
-    displayModal.value = true;
+  displayModal.value = true
 }
 const addContainerType = () => {
-    showModal.value = true;
+  showModal.value = true
 }
 const closeModal = () => {
-    showModal.value = false;
-    displayModal.value = false;
+  showModal.value = false
+  displayModal.value = false
 }
 
 const formState = reactive({
@@ -135,107 +166,108 @@ const formState = reactive({
   barcode: '',
   measurement: '',
   containerTypeCapacity: ''
-});
+})
 
-const measurements = ref([]);
-const products = ref([]);
-const containerTypes = ref([]);
-const containerTypeCapacities = ref([]);
-const selectedContainerType = ref(null);
+const measurements = ref([])
+const products = ref([])
+const containerTypes = ref([])
+const containerTypeCapacities = ref([])
+const selectedContainerType = ref(null)
 
 // Fetch data on mounted
 onMounted(async () => {
-  await fetchMeasurements();
-  await fetchProducts();
-  await fetchContainerTypes();
-});
+  await fetchMeasurements()
+  await fetchProducts()
+  await fetchContainerTypes()
+})
 
 const fetchMeasurements = async () => {
   try {
-    const response = await apiService.get('/measurements');
+    const response = await apiService.get('/measurements')
     console.log(response)
     catchAxiosSuccess(response)
-    measurements.value = response;
+    measurements.value = response
     console.log(measurements.value)
   } catch (error) {
     catchAxiosError(error)
-    console.error('Error fetching products:', error);
+    console.error('Error fetching products:', error)
   }
-};
+}
 const fetchProducts = async () => {
   try {
-    const response = await apiService.get('/all-products');
+    const response = await apiService.get('/all-products')
     console.log(response)
-    products.value = response;
+    products.value = response
     console.log(products.value)
   } catch (error) {
     catchAxiosError(error)
-    console.error('Error fetching products:', error);
+    console.error('Error fetching products:', error)
   }
-};
+}
 // console.log(products)
 const fetchContainerTypes = async () => {
   try {
-    const response = await apiService.get('/list-all-containers');
+    const response = await apiService.get('/list-all-containers')
     console.log(response.data)
-    containerTypes.value = response.data;
+    containerTypes.value = response.data
   } catch (error) {
-    console.error('Error fetching container types:', error);
-    catchAxiosError(error);
+    console.error('Error fetching container types:', error)
+    catchAxiosError(error)
   }
-};
+}
 
 const fetchContainerTypeCapacities = async () => {
   if (selectedContainerType.value) {
     try {
-      const response = await apiService.get(`/container-with-capacities/${selectedContainerType.value}`);
+      const response = await apiService.get(
+        `/container-with-capacities/${selectedContainerType.value}`
+      )
       console.log(response.data.container_capacities)
-      containerTypeCapacities.value = response.data.container_capacities;
+      containerTypeCapacities.value = response.data.container_capacities
     } catch (error) {
-      console.error('Error fetching container type capacities:', error);
+      console.error('Error fetching container type capacities:', error)
     }
   }
-};
+}
 
 const handleImageChange = (event) => {
-  const file = event.target.files[0];
+  const file = event.target.files[0]
   if (file) {
-    formState.productTypeImage = file;
-    const reader = new FileReader();
+    formState.productTypeImage = file
+    const reader = new FileReader()
     reader.onload = (e) => {
-      newImage.value = e.target.result;
-    };
-    reader.readAsDataURL(file);
+      newImage.value = e.target.result
+    }
+    reader.readAsDataURL(file)
   }
-};
+}
 
 const handleSubmit = async () => {
-  isSubmitting.value = true;
+  isSubmitting.value = true
 
-  const formData = new FormData();
-  formData.append('product_id', formState.product);
-  formData.append('product_type_name', formState.productTypeName);
-  formData.append('product_type_image', formState.productTypeImage);
-  formData.append('product_type_description', formState.productTypeDescription);
-  formData.append('barcode', formState.barcode);
-  formData.append('measurement_id', formState.measurement);
-  formData.append('container_type_capacity_id', formState.containerTypeCapacity);
+  const formData = new FormData()
+  formData.append('product_id', formState.product)
+  formData.append('product_type_name', formState.productTypeName)
+  formData.append('product_type_image', formState.productTypeImage)
+  formData.append('product_type_description', formState.productTypeDescription)
+  formData.append('barcode', formState.barcode)
+  formData.append('measurement_id', formState.measurement)
+  formData.append('container_type_capacity_id', formState.containerTypeCapacity)
 
   try {
-    let res = await apiService.post('/product-types', formData);
-    router.push('/product-type');
-    
-    catchAxiosSuccess(res);
-    return res;
+    let res = await apiService.post('/product-types', formData)
+    router.push('/product-type')
+
+    catchAxiosSuccess(res)
+    return res
   } catch (error) {
     catchAxiosError(error)
-    console.error('Error submitting form:', error);
+    console.error('Error submitting form:', error)
   } finally {
-    isSubmitting.value = false;
+    isSubmitting.value = false
   }
-};
+}
 </script>
-
 
 <style scoped>
 .container {
