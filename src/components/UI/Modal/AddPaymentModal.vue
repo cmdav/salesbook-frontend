befb49d40efb6643989dd3dabc3dfccd0c6b0bf8
radiant-o <bukoyeolaniyi@gmail.com>
2024-09-30 21:39:15 +0100

    updated

--- src/components/UI/Modal/AddPaymentModal.vue
+++ src/components/UI/Modal/AddPaymentModal.vue
@@ -60,11 +60,11 @@
             />
           </div>
           <div class="mb-4">
-            <label  class="block text-sm font-medium text-gray-700 pb-1">Payment Identifier</label>
+            <label  class="block text-sm font-medium text-gray-700 pb-1">Payment Method</label>
             <input
               type="text"
               v-model="form.paymentIdentifier"
-              placeholder="Enter Payment Identifier"
+              placeholder="Enter Payment Method"
               required
               class="mt-1 block w-[90%] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
